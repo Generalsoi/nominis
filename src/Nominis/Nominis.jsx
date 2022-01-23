6a45bec1e4822ce0494d48a684bae48ce4a27263
Generalsoi <ikhinobelesuccess@gmail.com>
2022-01-23 19:56:00 +0100

    updates to the nominis task

--- src/Nominis/Nominis.jsx
+++ src/Nominis/Nominis.jsx
@@ -3,7 +3,31 @@ import "./Nominis.css";
 import Header from "../Header/Header";
 import Icon3 from "../Assets/Images/Icono3.png";
 
+const shuffle = (array) => {
+  let currentIndex = array.length,
+    randomIndex;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+
+  return array;
+};
+
 const Nominis = () => {
+  const word = "I brush my teeth";
+  const wordList = word.split(" ");
+  const newWordList = shuffle(wordList);
+
   return (
     <div className="nominis">
       <Header />
@@ -23,6 +47,11 @@ const Nominis = () => {
               <img src={Icon3} alt="Icon3" />
             </div>
             <div className="input-bar"></div>
+            <div className="word-list-div">
+              {newWordList.map((el) => (
+                <div>{el}</div>
+              ))}
+            </div>
           </div>
         </div>
       </div>
